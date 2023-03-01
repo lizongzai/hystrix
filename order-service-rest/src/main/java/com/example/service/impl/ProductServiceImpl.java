f92181c1207ca2a79933ca832e151291c2060c7b
root <lizongzai@gmail.com>
2023-03-01 12:55:54 +0800

    V5 信号量服务隔离

--- order-service-rest/src/main/java/com/example/service/impl/ProductServiceImpl.java
+++ order-service-rest/src/main/java/com/example/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.pojo.Product;
 import com.example.service.IProductService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Resource;
@@ -32,30 +33,61 @@ public class ProductServiceImpl implements IProductService {
   @Autowired
   private LoadBalancerClient loadBalancerClient; //Ribbon负载均衡器
 
+//  /**
+//   * 功能描述: 查询商品列表
+//   *
+//   * @return
+//   */
+//  // 声明需要服务容错的方法
+//  // 线程池隔离
+//  @HystrixCommand(groupKey = "order-productService-listPool",// 服务名称，相同名称使用同一个线程池
+//      commandKey = "selectProductList",// 接口名称，默认为方法名
+//      threadPoolKey = "order-productService-listPool",// 线程池名称，相同名称使用同一个线程池
+//      commandProperties = {
+//          // 超时时间，默认 1000ms
+//          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+//      },
+//      threadPoolProperties = {
+//          // 线程池大小
+//          @HystrixProperty(name = "coreSize", value = "6"),
+//          // 队列等待阈值(最大队列长度，默认 -1)
+//          @HystrixProperty(name = "maxQueueSize", value = "100"),
+//          // 线程存活时间，默认 1min
+//          @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+//          // 超出队列等待阈值执行拒绝策略
+//          @HystrixProperty(name = "queueSizeRejectionThreshold", value = "100")
+//      }, fallbackMethod = "selectProductListFallback") // 托底数据
+//  @Cacheable(cacheNames = "orderService:product:list")
+//  @Override
+//  public List<Product> selectProductList() {
+//    System.out.println(
+//        Thread.currentThread().getName() + "-----orderService-----selectProductList-----");
+//    return restTemplate.exchange("http://product-service/product/list",
+//        HttpMethod.GET,
+//        null,
+//        new ParameterizedTypeReference<List<Product>>() {
+//        }).getBody();
+//  }
+//
+
   /**
    * 功能描述: 查询商品列表
    *
    * @return
    */
-  // 声明需要服务容错的方法
-  // 线程池隔离
-  @HystrixCommand(groupKey = "order-productService-listPool",// 服务名称，相同名称使用同一个线程池
-      commandKey = "selectProductList",// 接口名称，默认为方法名
-      threadPoolKey = "order-productService-listPool",// 线程池名称，相同名称使用同一个线程池
-      commandProperties = {
-          // 超时时间，默认 1000ms
-          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
-      },
-      threadPoolProperties = {
-          // 线程池大小
-          @HystrixProperty(name = "coreSize", value = "6"),
-          // 队列等待阈值(最大队列长度，默认 -1)
-          @HystrixProperty(name = "maxQueueSize", value = "100"),
-          // 线程存活时间，默认 1min
-          @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
-          // 超出队列等待阈值执行拒绝策略
-          @HystrixProperty(name = "queueSizeRejectionThreshold", value = "100")
-      }, fallbackMethod = "selectProductListFallback") // 托底数据
+// 声明需要服务容错的方法
+  // 信号量隔离
+  @HystrixCommand(commandProperties = {
+      // 超时时间，默认 1000ms
+      @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+          value = "5000"),
+      // 信号量隔离
+      @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_STRATEGY,
+          value = "SEMAPHORE"),
+      // 信号量最大并发，调小一些方便模拟高并发
+      @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_SEMAPHORE_MAX_CONCURRENT_REQUESTS,
+          value = "6")
+  }, fallbackMethod = "selectProductListFallback")
   @Cacheable(cacheNames = "orderService:product:list")
   @Override
   public List<Product> selectProductList() {
@@ -67,6 +99,7 @@ public class ProductServiceImpl implements IProductService {
         new ParameterizedTypeReference<List<Product>>() {
         }).getBody();
   }
+
 
   /**
    * 功能描述: 根据多个主键查询商品
