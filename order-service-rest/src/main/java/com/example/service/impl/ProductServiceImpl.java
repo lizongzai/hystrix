93cfb5132edfca36599ff5535f74a04856678427
root <lizongzai@gmail.com>
2023-03-01 13:23:49 +0800

    V6 服务熔断处理

--- order-service-rest/src/main/java/com/example/service/impl/ProductServiceImpl.java
+++ order-service-rest/src/main/java/com/example/service/impl/ProductServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.service.IProductService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Resource;
@@ -118,6 +120,40 @@ public class ProductServiceImpl implements IProductService {
         List.class);
   }
 
+//  /**
+//   * 功能描述: 根据主键查询商品
+//   *
+//   * @param id
+//   * @return
+//   */
+//  // 声明需要服务容错的方法
+//  // 线程池隔离
+//  @HystrixCommand(groupKey = "order-productService-singlePool",// 服务名称，相同名称使用同一个线程池
+//      commandKey = "selectProductById",// 接口名称，默认为方法名
+//      threadPoolKey = "order-productService-singlePool",// 线程池名称，相同名称使用同一个线程池
+//      commandProperties = {
+//          // 超时时间，默认 1000ms
+//          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+//              value = "5000")
+//      },
+//      threadPoolProperties = {
+//          // 线程池大小
+//          @HystrixProperty(name = "coreSize", value = "3"),
+//          // 队列等待阈值(最大队列长度，默认 -1)
+//          @HystrixProperty(name = "maxQueueSize", value = "100"),
+//          // 线程存活时间，默认 1min
+//          @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+//          // 超出队列等待阈值执行拒绝策略
+//          @HystrixProperty(name = "queueSizeRejectionThreshold", value = "100")
+//      })
+//  @Cacheable(cacheNames = "orderService:product:single", key = "#id")
+//  @Override
+//  public Product selectProductById(Integer id) {
+//    System.out.println(
+//        Thread.currentThread().getName() + "-----orderService-----selectProductById-----");
+//    return restTemplate.getForObject("http://product-service/product/" + id, Product.class);
+//  }
+
   /**
    * 功能描述: 根据主键查询商品
    *
@@ -125,30 +161,25 @@ public class ProductServiceImpl implements IProductService {
    * @return
    */
   // 声明需要服务容错的方法
-  // 线程池隔离
-  @HystrixCommand(groupKey = "order-productService-singlePool",// 服务名称，相同名称使用同一个线程池
-      commandKey = "selectProductById",// 接口名称，默认为方法名
-      threadPoolKey = "order-productService-singlePool",// 线程池名称，相同名称使用同一个线程池
-      commandProperties = {
-          // 超时时间，默认 1000ms
-          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
-              value = "5000")
-      },
-      threadPoolProperties = {
-          // 线程池大小
-          @HystrixProperty(name = "coreSize", value = "3"),
-          // 队列等待阈值(最大队列长度，默认 -1)
-          @HystrixProperty(name = "maxQueueSize", value = "100"),
-          // 线程存活时间，默认 1min
-          @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
-          // 超出队列等待阈值执行拒绝策略
-          @HystrixProperty(name = "queueSizeRejectionThreshold", value = "100")
-      })
-  @Cacheable(cacheNames = "orderService:product:single", key = "#id")
+  // 服务熔断
+  @HystrixCommand(commandProperties = {
+      // 当请求符合熔断条件触发 fallbackMethod 默认 20 个
+      @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_REQUEST_VOLUME_THRESHOLD, value = "10"),
+      // 请求错误率大于 50% 就启动熔断器，然后 for 循环发起重试请求，当请求符合熔断条件触发 fallbackMethod
+      @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_ERROR_THRESHOLD_PERCENTAGE, value = "50"),
+      // 熔断多少秒后去重试请求，默认 5s
+      @HystrixProperty(name = HystrixPropertiesManager.CIRCUIT_BREAKER_SLEEP_WINDOW_IN_MILLISECONDS, value = "5000"),
+  }, fallbackMethod = "selectProductByIdFallback")
   @Override
   public Product selectProductById(Integer id) {
-    System.out.println(
-        Thread.currentThread().getName() + "-----orderService-----selectProductById-----");
+    //System.out.println(Thread.currentThread().getName() + "-----orderService-----selectProductById-----");
+    System.out.println("-----selectProductById-----" + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+
+    // 模拟查询主键为 1 的商品信息会导致异常
+    if (1 == id) {
+      throw new RuntimeException("查询主键为 1 的商品信息导致异常");
+    }
+
     return restTemplate.getForObject("http://product-service/product/" + id, Product.class);
   }
 
@@ -161,6 +192,11 @@ public class ProductServiceImpl implements IProductService {
         new Product(2, "托底数据-联想笔记本", 1, 6888D),
         new Product(3, "托底数据-小米平板", 5, 2020D)
     );
+  }
+
+  // 托底数据
+  private Product selectProductByIdFallback(Integer id) {
+    return new Product(id, "托底数据", 1, 9999D);
   }
 
 }
